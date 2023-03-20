2373e4e0f64a70bb7ba0061efb9883470f46e6c1
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-03-19 23:21:50 -0700

    ready for next stage

--- epbwn/main.js
+++ epbwn/main.js
@@ -1,1 +1,21 @@
-
+const swiper = new Swiper('.swiper', {
+ 
+    // direction: 'vertical',
+   
+    loop: true,
+  
+    
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
