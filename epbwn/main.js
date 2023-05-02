a4753c83d6f42d08196ef0f30b20b1ee58097412
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-05-01 18:33:11 -0700

    kkk

--- epbwn/main.js
+++ epbwn/main.js
@@ -16,7 +16,31 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+  
   });
+
+
+  function adjustCarousel(){
+    const activeIndex = swiper.activeIndex;
+const activeSlide = swiper.slides[activeIndex];
+const activeWidth = activeSlide.offsetWidth;
+const activeHeight = activeSlide.offsetHeight;
+
+const swiperContainer = document.querySelector('.swiper');
+swiperContainer.style.width = activeWidth + 'px';
+swiperContainer.style.height = activeHeight + 'px';
+console.log('dis funciton was read')
+console.log(activeIndex , activeSlide , activeWidth ,activeHeight)
+  } 
+
+
+
+const right = document.querySelector('.swiper-button-next');
+const left = document.querySelector('.swiper-button-prev');
+
+left.addEventListener('click' , adjustCarousel)
+right.addEventListener('click' , adjustCarousel)
+
+window.addEventListener('load' , adjustCarousel)
+
+  
